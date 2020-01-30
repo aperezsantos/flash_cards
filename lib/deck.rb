9dffce6c89a55d4c34c7a32bfb18024ae78e8b9a
Ana Perez Santos <a.jperez@yahoo.com>
2020-01-29 17:30:05 -0700

    Iteration 2: refactor cards_in_category

--- lib/deck.rb
+++ lib/deck.rb
@@ -10,16 +10,7 @@ class Deck
   end
 
   def cards_in_category(category)
-    #take the deck of cards
-    #search for all the cards that belong to given category
-    #try to use each then a different enumerable
-    stem_cards = []
-    @cards.each do |card|
-      if card.category == category
-        stem_cards << card
-      end
-    end
-    stem_cards
+    @cards.find_all { |card| card.category == category }
   end
 
 end
