f5533baf637eedc21a697855abe16ab8f50633d8
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-02 01:24:10 -0700

    RunnerFile Iteration 3: build input/output messages, complete

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -19,6 +19,40 @@ def start
   @deck = Deck.new(@cards)
   @round = Round.new(@deck)
   @turns = [@turn1, @turn2, @turn3]
+
+  puts "------------------------------------"
+  puts "Welcome! You're playing with 3 cards."
+  puts "------------------------------------"
+  puts " "
+  puts "This is card number 1 out of 3."
+  puts " "
+  puts "Question: #{@round.current_card.question}"
+  puts "Guess:"
+  @round.take_turn(gets.chomp)
+  puts @round.turns.last.feedback
+  puts "------------------------------------"
+  puts " "
+  puts "This is card number 2 out of 3."
+  puts " "
+  puts "Question: #{@round.current_card.question}"
+  puts "Guess:"
+  @round.take_turn(gets.chomp)
+  puts @round.turns.last.feedback
+  puts "------------------------------------"
+  puts " "
+  puts "This is card number 3 out of 3."
+  puts " "
+  puts "Question: #{@round.current_card.question}"
+  puts "Guess:"
+  @round.take_turn(gets.chomp)
+  puts @round.turns.last.feedback
+  puts "------------------------------------"
+  puts "****** Thank you for playing! ******"
+  puts "*********** Your Results ***********"
+  puts "------------------------------------"
+  puts "You had #{@round.number_correct} correct guesses out of 3 for a total score of #{@round.percent_correct}%"
+  puts "Geography - #{@round.percent_correct_by_category(:Geography)}%"
+  puts "STEM - #{@round.percent_correct_by_category(:STEM)}%"
 end
 
 puts start
