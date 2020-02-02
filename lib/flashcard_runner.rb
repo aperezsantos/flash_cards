dd55d6e0d90d26753f71b434e9b1a210b3e77d5a
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-02 01:11:09 -0700

    RunnerFile Iteration 3: create runner file

---
+++ lib/flashcard_runner.rb
@@ -0,0 +1,11 @@
+# Inside of this file, write the code to do the following:
+#
+# Create some Cards
+# Put those card into a Deck
+# Create a new Round using the Deck you created
+# Start the round using a new method called start
+
+require './lib/round'
+require './lib/turn'
+require './lib/deck'
+require './lib/card'
