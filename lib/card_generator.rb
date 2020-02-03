bdf068910c9c428aa48b5c1c54b11d3b9e287199
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-02 19:30:13 -0700

    Iteration 4 Card Generator: initialize with file path

--- lib/card_generator.rb
+++ lib/card_generator.rb
@@ -1,6 +1,8 @@
 class CardGenerator
+  attr_reader :file
 
   def initialize(file_path)
-    
+    @file = file_path
   end
+
 end
--- test/card_generator_test.rb
+++ test/card_generator_test.rb
@@ -1,13 +1,20 @@
 require 'minitest/autorun'
 require 'minitest/pride'
 require './lib/card_generator'
+require './lib/card'
 
 class CardGeneratorTest < Minitest::Test
 
   def test_it_exists
-    cards = CardGenerator.new("lib/cards.txt")
+    cards = CardGenerator.new("cards.txt")
 
     assert_instance_of CardGenerator, cards
+  end
+
+  def test_it_has_file_path
+    cards = CardGenerator.new("cards.txt")
+
+    assert_equal "cards.txt", cards.file
   end
 
 end
