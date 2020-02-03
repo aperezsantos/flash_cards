8a957df6af5ed9fa332bf87482b070a7be5ca5f6
Ana Perez Santos <a.jperez@yahoo.com>
2020-02-02 22:48:43 -0700

    Refactor: introduce card generator to runner file, WIP: refactor output/input

--- lib/flashcard_runner.rb
+++ lib/flashcard_runner.rb
@@ -2,19 +2,18 @@ require './lib/round'
 require './lib/turn'
 require './lib/deck'
 require './lib/card'
+require './lib/card_generator'
 
 
 def start
-  @card_1 = Card.new("What is the capital of Alaska?", "Juneau", :Geography)
-  @card_2 = Card.new("The Viking spacecraft sent back to Earth photographs and reports about the surface of which planet?", "Mars", :STEM)
-  @card_3 = Card.new("Describe in words the exact direction that is 697.5° clockwise from due north?", "North north west", :STEM)
-  @cards = [@card_1, @card_2, @card_3]
-  @deck = Deck.new(@cards)
+  @card_generator = CardGenerator.new("./lib/cards.txt")
+  @deck = Deck.new(@card_generator.cards)
   @round = Round.new(@deck)
-  @turns = [@turn1, @turn2, @turn3]
+
+  require "pry"; binding.pry
 
   puts "------------------------------------"
-  puts "Welcome! You're playing with 3 cards."
+  puts "Welcome! You're playing with #{@deck.cards.length} cards."
   puts "------------------------------------"
   puts " "
   puts "This is card number 1 out of 3."
